526159f4046555e4b958146c6dd417ab355f0458
muhammad suhail <suhailpk539@gmail.com>
2024-04-29 12:23:21 +0530

    connected to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import mongoose from 'mongoose'
 
  dotenv.config()
+ mongoose.connect("mongodb://localhost:27017/backendecommerce")
 const app=express()
     const PORT=process.env.PORT||7000
 
