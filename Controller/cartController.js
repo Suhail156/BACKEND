5aa0fa5e2d0b30c14cb3e79900d0636051d192bf
muhammad suhail <suhailpk539@gmail.com>
2024-05-03 19:32:58 +0530

    added decrement

--- Controller/cartController.js
+++ Controller/cartController.js
@@ -103,3 +103,80 @@ export const addCartQuantity=async(req,res,next)=>{
         next(error)
     }
 }
+
+export const decremntQuantity=async(req,res,next)=>{
+  try {
+    const userId=req.params.userid
+    const productId=req.params.productid
+    const{quantityDecrement}=req.body
+
+    //find user by id
+    const user=await User.findById(userId)
+    console.log(user);
+    if(!user){
+      res.status(404).json({message:"user not found"})
+    }
+    // find product by id
+    const product=await Products.findById(productId)
+    console.log(product);
+    if(!product){
+      res.status(404).json({message:"product not found"})
+    }
+    //find or create item
+    let cartItem = await Cart.findOne({ userId: user._id, productId: product._id });
+    if (cartItem) {
+        // If the product already exists, decrement the quantity
+        if(typeof quantityDecrement !== "number"){
+            return res.status(400).json({message: "Bad request"})
+        }
+        if(cartItem.quantity - quantityDecrement >= 0){
+            cartItem.quantity -= quantityDecrement;
+            await cartItem.save();
+        }
+        else{
+            cartItem.quantity = 1
+            await cartItem.save();
+        }
+        
+    }
+    res.status(201).json({ message: "Quantity decremented" });
+} catch (error) {
+    next(error)
+}
+}
+
+export const removeCart=async(req,res,next)=>{
+  try {
+    const{userId,productId}=req.params
+
+   //find by user
+ const user=await User.findById(userId)
+ console.log(userId);
+ if(!user){
+  res.status(404).json({message:"user not found"})
+ }
+
+ //find by product
+ const product=await Products.findById(productId)
+ if(!product){
+  res.status(404).json({message:"product not found"})
+ }
+ const cartItem=await Cart.findOneAndDelete({userId:user._id,productId:product._id})
+  if(!cartItem){
+    res.status(404).json({message:"product not found in the user cart"})
+  }
+  //ind the index of the cart item in the user's cartItems array
+  const cartItemIndex=user.cart.findIndex(item=>item.equals(cartItem._id))
+
+   //cart item found,remove from cartitems array
+
+   if(cartItemIndex !== -1){
+    user.cart.splice(cartItemIndex,1)
+    await user.save()
+    res.status(200).json({message:"product removed successfully"})
+   }
+  } catch (error) {
+    next(error)
+  }
+   
+}
--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { productByCategory, productById, viewProduct } from '../Controller/productController.js'
 import { verifytoken } from '../middlewares/authMiddleware.js'
-import { addCartQuantity, addToCart, viewCart } from '../Controller/cartController.js'
+import { addCartQuantity, addToCart, decremntQuantity, removeCart, viewCart } from '../Controller/cartController.js'
 
 const router=express.Router()
 //products 
@@ -12,6 +12,8 @@ router.get("/products/category/:categoryname",verifytoken,productByCategory)
  //cart 
  router.post("/:userid/cart/:productid",verifytoken,addToCart)
  router.get("/:id/cart",verifytoken,viewCart)
- router.patch("/:userid/cart/:productid/increment",addCartQuantity)
+ router.patch("/:userid/cart/:productid/increment",verifytoken,addCartQuantity)
+ router.patch("/:userid/cart/:productid/decrement",verifytoken,decremntQuantity)
+ router.delete("/:userId/cart/:productId/remove",removeCart)
 
 export default router   
