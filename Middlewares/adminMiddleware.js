33d08a8a2e5b3683e54f6da57c3b9956ca713f39
muhammad suhail <suhailpk539@gmail.com>
2024-05-07 19:54:41 +0530

    create admin jwt

---
+++ Middlewares/adminMiddleware.js
@@ -0,0 +1,23 @@
+import Jwt from "jsonwebtoken";
+import dotenv from 'dotenv'
+  
+dotenv.config()
+export const verifytoken=(req,res,next)=>{
+    try {
+        const token=req.headers["authorization"]
+    if(!token){
+        res.status(404).json({error:"token not provided"})
+    }
+    Jwt.verify(token,process.env.USER_SECRET_TOKEN,(err,decoded)=>{
+        if(err){
+            res.status(404).json({error:"unauthorized"})
+        }
+        req.email=decoded.email
+        next()
+    })
+    } catch (error) {
+      return next(error)  
+    }
+    
+    
+}
--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -3,6 +3,7 @@ import { productByCategory, productById, viewProduct } from '../Controller/produ
 import { verifytoken } from '../middlewares/authMiddleware.js'
 import { addCartQuantity, addToCart, decremntQuantity, removeCart, viewCart } from '../Controller/cartController.js'
 import { addToWishlist, removewishlist, viewWishlist } from '../Controller/wishlistController.js'
+import { payment } from '../Controller/userPaymentController.js'
 
 const router=express.Router()
 //products 
@@ -21,3 +22,6 @@ router.post("/:userid/wishlist/:productid",addToWishlist)
 router.get("/:id/wishlist",viewWishlist)
 router.delete("/:userid/wishlist/:productid/remove",removewishlist)
 export default router   
+
+//payment
+router.post("/:userid/payment",payment)
