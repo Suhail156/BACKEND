d632b5709eb8b266af4205832f018f2a1be8369f
muhammad suhail <suhailpk539@gmail.com>
2024-05-17 12:03:24 +0530

    .

--- Middlewares/authMIddleware.js
+++ Middlewares/authMIddleware.js
@@ -1,43 +1,3 @@
-// import Jwt from "jsonwebtoken";
-// import dotenv from 'dotenv'
-  
-// dotenv.config()
-// export const verifytoken=(req,res,next)=>{
-//     try {
-//         const token=req.headers["authorization"]
-//     if(!token){
-//         res.status(404).json({error:"user not found"})
-//     }
-//     Jwt.verify(token,process.env.USER_SECRET_TOKEN,(err,decoded)=>{
-//         if(err){
-//             if(err.message==='jwt expired'){
-//                 const refreshToken = req.headers['refresh_token']
-//               console.log("hii",token);
-//             if(!refreshToken){
-//                 return res.status(401).json('Refresh token is required')
-//               }
-//               Jwt.verify(refreshToken,process.env.REFRESH_ACCESS_TOKEN,(err,decoded)=>{
-//                 if(err){
-//                 res.status(404).json({message:"invalid refresh token"})
-//                 }
-//                 const refreshToken=Jwt.sign({id:uservalid._id},process.env.REFRESH_ACCESS_TOKEN,{expiresIn:"10m"})
-//                 req.email=decoded.email
-//                  res.cookies("authorization",refreshToken)
-//                  next()
-//                  return
-//               })
-//             }
-//             res.status(404).json({error:"unauthorized"})
-//         }
-//         req.email=decoded.email
-//         next()
-//     })
-//     } catch (error) {
-//       return next(error)  
-//     } 
-// }
-
-
 import Jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
 
@@ -83,3 +43,64 @@ export const verifytoken = (req, res, next) => {
         return next(error);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Jwt from "jsonwebtoken";
+// import dotenv from 'dotenv'
+  
+// dotenv.config()
+// export const verifytoken=(req,res,next)=>{
+//     try {
+//         const token=req.headers["authorization"]
+//     if(!token){
+//         res.status(404).json({error:"user not found"})
+//     }
+//     Jwt.verify(token,process.env.USER_SECRET_TOKEN,(err,decoded)=>{
+//         if(err){
+//             if(err.message==='jwt expired'){
+//                 const refreshToken = req.headers['refresh_token']
+//               console.log("hii",token);
+//             if(!refreshToken){
+//                 return res.status(401).json('Refresh token is required')
+//               }
+//               Jwt.verify(refreshToken,process.env.REFRESH_ACCESS_TOKEN,(err,decoded)=>{
+//                 if(err){
+//                 res.status(404).json({message:"invalid refresh token"})
+//                 }
+//                 const refreshToken=Jwt.sign({id:uservalid._id},process.env.REFRESH_ACCESS_TOKEN,{expiresIn:"10m"})
+//                 req.email=decoded.email
+//                  res.cookies("authorization",refreshToken)
+//                  next()
+//                  return
+//               })
+//             }
+//             res.status(404).json({error:"unauthorized"})
+//         }
+//         req.email=decoded.email
+//         next()
+//     })
+//     } catch (error) {
+//       return next(error)  
+//     } 
+// }
